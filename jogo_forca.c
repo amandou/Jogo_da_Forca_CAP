2901e245252cbc4dc1c3c89fd6494c8f07347fa4
VanderleiJr <vanderlei.brito.junior@gmail.com>
2017-07-07 20:31:28 -0300

    Criação de cadastro das fichas dos jogadores com struct

--- jogo_forca.c
+++ jogo_forca.c
@@ -5,5 +5,29 @@
 
 struct jogador{
   char nome[50];
-  int ra, jogadas;
+  int ra, partidas;
 };
+
+int main(){
+
+  struct jogador jog;
+  char *pLink;
+  char resposta[1000] = {'\n'};
+  FILE *pJogadores;
+
+  pLink = getenv("QUERY_STRING");
+  if (pLink == NULL)
+    snprintf(resposta, sizeof resposta, "Ocorreu um erro!\n");
+  else if(sscanf(pLink, "nome=%[^\n]&ra=%d", jog.nome, &jog.ra)){
+    if(jog.ra <= 0)
+      snprintf(resposta, sizeof resposta, "Digite um RA válido!");
+    else if(strlen(jog.nome) == 0)
+      snprintf(resposta, sizeof resposta, "Digite um nome!");
+    else
+      pJogadores = fopen("jogadores.txt", "a");
+      fprintf(pJogadores, "Nome: %s\n", jog.nome);
+      fprintf(pJogadores, "RA: %d\nPartidas: %d", jog.ra, jog.partidas=0);
+  }
+
+  return 0;
+}
